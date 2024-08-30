e94fc5820bd7f24e88d6fcf25bc7f5e8e53beec8
Bharat Kumawat <bharatkumawatk1@gmail.com>
2024-08-30 18:10:09 +0530

    change cors proxy

--- src/config.js
+++ src/config.js
@@ -13,8 +13,10 @@ export const IMG_CDN_URL = "https://www.swiggy.com/mapi/homepage/getCards?lat=26
 //     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.912185&lng=75.783304&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 //   );
 
-export const IMG_CDN_URL2 = 
-  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.912185&lng=75.783304&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+// export const IMG_CDN_URL2 = 
+//   "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.912185&lng=75.783304&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+  export const IMG_CDN_URL2 = 'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.912185&lng=75.783304&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
 
 
 
